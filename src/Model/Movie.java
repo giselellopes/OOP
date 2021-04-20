aebfcfa92fe1cb023a1dceba429632912c6245a3
giselellopes <69086061+giselellopes@users.noreply.github.com>
2021-04-20 15:11:17 +0100

    Exclusion classes, included card and customers information on Payment class.

--- src/Model/Movie.java
+++ src/Model/Movie.java
@@ -9,7 +9,7 @@ package Model;
  *
  * @author Home
  */
-abstract class Movie {
+public class Movie {
     
     protected int movieCod;
     protected String movieTitle;
@@ -17,20 +17,76 @@ abstract class Movie {
     protected double moviePrice;
     protected String movieGenre;
     protected Boolean movieStatus;
-    protected String MoviePic;
+    protected String moviePic;
     
     public Movie(){
         
     }
     
-    public Movie(int movieCod, String movieTitle, String movieDescription, double moviePrice, String movieGenre, Boolean movieStatus, String moviePic){
+    public Movie(int movieCod, String movieTitle, String movieDescription, double moviePrice, String movieGenre, Boolean movieStatus, String moviePic ){
         this.movieCod = movieCod;
         this.movieTitle = movieTitle;
         this.movieDescription = movieDescription;
         this.moviePrice = moviePrice;
         this.movieGenre = movieGenre; 
         this.movieStatus = movieStatus;
-        this.MoviePic = moviePic;
+        this.moviePic = moviePic;
     }
     
+    public int getmovieCod(){
+        return movieCod;
+    }
+    
+    public String getmovieTitle(){
+        return movieTitle;
+    } 
+
+    public String getmovieDescription(){
+        return movieDescription;
+    }
+    
+    public double getmoviePrice(){
+        return moviePrice;
+    } 
+    
+    public String getmovieGenre(){
+        return movieGenre;
+    }
+    
+    public Boolean getmovieStatus(){
+        return movieStatus;
+    } 
+    
+    public String getmoviePic(){
+        return moviePic;
+    }
+
+    public void setMovieCod(int movieCod) {
+        this.movieCod = movieCod;
+    }
+
+    public void setMovieTitle(String movieTitle) {
+        this.movieTitle = movieTitle;
+    }
+
+    public void setMovieDescription(String movieDescription) {
+        this.movieDescription = movieDescription;
+    }
+
+    public void setMoviePrice(double moviePrice) {
+        this.moviePrice = moviePrice;
+    }
+
+    public void setMovieGenre(String movieGenre) {
+        this.movieGenre = movieGenre;
+    }
+
+    public void setMovieStatus(Boolean movieStatus) {
+        this.movieStatus = movieStatus;
+    }
+
+    public void setMoviePic(String moviePic) {
+        this.moviePic = moviePic;
+    }
+        
 }
